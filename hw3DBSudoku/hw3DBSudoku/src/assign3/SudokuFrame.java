ddafb2ed3879b071bae2389d8178315592bbff20
ej2xu <yxu@willamette.edu>
2016-07-25 18:18:00 -0500

    layout GUI components

--- hw3DBSudoku/hw3DBSudoku/src/assign3/SudokuFrame.java
+++ hw3DBSudoku/hw3DBSudoku/src/assign3/SudokuFrame.java
@@ -9,11 +9,31 @@ import java.awt.event.*;
 
 
  public class SudokuFrame extends JFrame {
+	private JTextArea puzzle; 
+	private JTextArea solution; 
+	private JButton check;
+	private JCheckBox autoCheck;
 	
 	public SudokuFrame() {
 		super("Sudoku Solver");
+		setLayout(new BorderLayout(4,4));
 		
-		// YOUR CODE HERE
+		puzzle = new JTextArea(15, 20);
+		puzzle.setBorder(new TitledBorder("Puzzle"));
+		solution = new JTextArea(15, 20);
+		solution.setBorder(new TitledBorder("Solution"));
+		solution.setEditable(false);
+		
+		JPanel control = new JPanel();
+		control.setLayout(new BoxLayout(control, BoxLayout.X_AXIS));
+		check = new JButton("Check");
+		autoCheck = new JCheckBox("Auto Check");
+		control.add(check);
+		control.add(autoCheck);
+		
+		add(puzzle, BorderLayout.CENTER);
+		add(solution, BorderLayout.EAST);
+		add(control, BorderLayout.SOUTH);
 		
 		// Could do this:
 		// setLocationByPlatform(true);
