6b4b14e60cbf11e9b2cf4fe99d954b4f641fa459
ej2xu <yxu@willamette.edu>
2016-08-04 16:22:37 -0500

    finish cracker project

--- hw4/src/crackerAssign/Cracker.java
+++ hw4/src/crackerAssign/Cracker.java
@@ -1,11 +1,16 @@
 package crackerAssign;
 
 import java.security.MessageDigest; 
-import java.security.NoSuchAlgorithmException; 
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.concurrent.CountDownLatch;
+
+import bankAssign.Transaction; 
 
 public class Cracker {
 	// Array of chars used to produce strings
 	public static final char[] CHARS = "abcdefghijklmnopqrstuvwxyz0123456789.,-!".toCharArray();	
+	public static final int MAX_WORKERS = CHARS.length;
 	
 	/*
 	 Given a byte[] array, produces a hex String,
@@ -37,30 +42,82 @@ public class Cracker {
 		return result;
 	}
 	
-	private String genHash(String pw) {
+	public static byte[] genHash(String pw) {
 			MessageDigest md;
 			try {
 				md = MessageDigest.getInstance("SHA");
 				md.update(pw.getBytes());
-			    return hexToString(md.digest());	
+			    return md.digest();	
 			} catch (NoSuchAlgorithmException e) {
 				e.printStackTrace();
 			}		  	     	 
 		return null;
 	}
 	
-	public static void main(String[] args) {
-		Cracker cracker = new Cracker();
-		if (args.length == 0) {
-			System.out.println("Usage: Cracker password / Cracker pwHash max_length num_of_thread");
-			System.exit(1);
-		} else if (args.length == 1) {
-			System.out.println(cracker.genHash(args[0]));
-		} else if (args.length == 3) {
+	public static class Worker extends Thread {
+		int step;
+		int index;
+		int length;
+		byte[] hashBytes;
+		public Worker(int i, int s, int l, String hash, byte[] hb) {
+			index = i;
+			step = s;
+			length = l;
+			hashBytes = hb;
+		}
+
+		@Override
+		public void run() {
+			int start = index * step;
+			int tmp = (index + 1) * step;
+			int end = (tmp+step > MAX_WORKERS) ? MAX_WORKERS : tmp;
+			for (int i = start; i < end; i++)
+				crackHash("" + CHARS[i]);
 			
+			latch.countDown();
+		}
+		
+		public void crackHash(String result) {
+			if (result.length() <= length) {
+				if (Arrays.equals(hashBytes, genHash(result)))
+					System.out.println(result);
+				
+				for (char ch : CHARS)
+					crackHash(result + ch);
+			}
 		}
 	}
 	
+	static CountDownLatch latch;
+	
+	public static void main(String[] args) {
+		if (args.length == 1) {
+			System.out.println(hexToString(genHash(args[0])));
+		} else if (args.length == 3) {
+			try {
+				int maxLength = Integer.parseInt(args[1]);
+				int numOfWorkers = Integer.parseInt(args[2]);
+				byte[] hashBytes = hexToArray(args[0]);
+						
+				latch = new CountDownLatch(numOfWorkers);
+				int step = MAX_WORKERS / numOfWorkers;
+				for (int i = 0; i < numOfWorkers; i++)
+					new Worker(i, step, maxLength, args[0], hashBytes).start();
+				
+				try { latch.await(); }
+				catch (InterruptedException ignored) {}	
+				
+				System.out.println("All done");
+			} catch (Exception ignored) { hintAndExit(); }
+		} else {
+			hintAndExit();
+		}
+	}
+	
+	private static void hintAndExit() {
+		System.out.println("Usage: Cracker password / Cracker pwHash max_length num_of_thread");
+		System.exit(1);
+	}
 	// possible test values:
 	// a 86f7e437faa5a7fce15d1ddcb9eaeaea377667b8
 	// fm adeb6f2a18fe33af368d91b09587b68e3abcb9a7
