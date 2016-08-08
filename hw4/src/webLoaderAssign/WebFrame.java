5c2ba4763b1f0ffcf670fdf207a71d95a9f7c7f4
ej2xu <yxu@willamette.edu>
2016-08-08 00:51:35 -0500

    finish hw4

--- hw4/src/webLoaderAssign/WebFrame.java
+++ hw4/src/webLoaderAssign/WebFrame.java
@@ -1,34 +1,48 @@
 package webLoaderAssign;
 
-import java.awt.*;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
+import java.awt.Dimension;
 import java.awt.event.*;
 import java.io.*;
 import java.util.*;
+import java.util.concurrent.*;
 import java.util.concurrent.atomic.*;
 
 
 public class WebFrame extends JFrame {
+	private static final String FILENAME = System.getProperty("user.dir") + "/bin/links.txt";
 	private DefaultTableModel model;
 	private JTable table;
 	// in top to button order on the GUI
 	private JButton single;
 	private JButton concurrent;
-	private JTextField numOfWorker;
+	private JTextField numConcurrent;
 	private JLabel running;
 	private JLabel completed;
 	private JLabel elapsed;
 	private JProgressBar progress;
 	private JButton stop;
 	
+	private List<WebWorker> workers;
+	private List<String> urls;
+	public Launcher launcher;
+	private int numWorkers;
+	public AtomicInteger runCount;
+    private AtomicInteger completedCount;
+	private Semaphore workerSemaphore;
+    private long startTime;
+	
 	public WebFrame() {
 		super("WebLoader");
+		launcher = null;
+		urls = new ArrayList<String>();
 		JPanel panel = (JPanel) getContentPane();
 		
 		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
 		
 		model = new DefaultTableModel(new String[] {"url", "status"}, 0);
+		loadFile();
 		table = new JTable(model);
 		table.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
 		
@@ -40,39 +54,151 @@ public class WebFrame extends JFrame {
 		add(single);
 		single.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
+				launcher = new Launcher(1);
+				launch();
 			}
 		});		
 		concurrent = new JButton("Concurrent Fetch");
 		add(concurrent);
-		single.addActionListener(new ActionListener() {
+		concurrent.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
+				launcher = new Launcher(Integer.parseInt(numConcurrent.getText()));
+				launch();	
 			}
 		});
-		numOfWorker = new JTextField("4", 4);
-		numOfWorker.setMaximumSize(numOfWorker.getPreferredSize());
-		add(numOfWorker);
+		numConcurrent = new JTextField("4", 4);
+		numConcurrent.setMaximumSize(numConcurrent.getPreferredSize());
+		add(numConcurrent);
 		running = new JLabel("Running:");
 		add(running);
 		completed = new JLabel("Completed:");
 		add(completed);
 		elapsed = new JLabel("Elapsed:");
 		add(elapsed);
-		progress = new JProgressBar(0, model.getRowCount());
+		progress = new JProgressBar(0, urls.size());
+		progress.setValue(0);
 		add(progress);
 		stop = new JButton("Stop");
 		stop.setEnabled(false);
 		add(stop);
 		stop.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
+				launcher.interrupt();
+				for (WebWorker w : workers)
+					w.interrupt();
+				stop();
 			}
 		});		
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		pack();
 		setVisible(true);
+	}
+	
+	private void launch() {
+		single.setEnabled(false);
+		concurrent.setEnabled(false);
+		running.setText("Running:");
+		completed.setText("Completed:");
+		elapsed.setText("Elapsed:");
+		startTime = System.currentTimeMillis();
+		launcher.start();
+		stop.setEnabled(true);
+		for (int i = 0; i < urls.size(); i++)
+			model.setValueAt("", i, 1);
+	}
+	
+	private void stop() {
+		single.setEnabled(true);
+		concurrent.setEnabled(true);
+		stop.setEnabled(false);
+		progress.setValue(0);
+	}
+	
+	public void releaseWorker(String status, int rowToUpdate) {
+		runCount.decrementAndGet();
+		completedCount.incrementAndGet();
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				running.setText("Running:" + runCount);
+				completed.setText("Completed:" + completedCount);
+				progress.setValue(completedCount.get());
+				model.setValueAt(status, rowToUpdate, 1);
+			}
+		});
+		workerSemaphore.release();
+		checkDone();
+	}
+	
+	private void checkDone() {
+		long timeElapsed = System.currentTimeMillis() - startTime;
+		if (runCount.get() == 0) {
+			SwingUtilities.invokeLater(new Runnable() {
+				public void run() {
+					stop();
+					elapsed.setText("Elapsed:" + timeElapsed);
+				}
+			});
+		}
+	}
+	
+	public class Launcher extends Thread {
+		private WebFrame frame;
+		
+		public Launcher(int numConcurrent) {
+			workerSemaphore = new Semaphore(numConcurrent);
+			workers = new ArrayList<WebWorker>();
+			runCount = new AtomicInteger(0);
+			completedCount = new AtomicInteger(0);
+			frame = WebFrame.this;
+		}
+		
+		public void run() {
+			runCount.incrementAndGet();
+			showRun();
+			for (int i = 0; i < numWorkers; i++) {
+				try {
+					workerSemaphore.acquire();
+					WebWorker worker = new WebWorker(urls.get(i), i, frame);
+					workers.add(worker);
+					worker.start();
+				}
+				catch (InterruptedException e) {
+					break;
+				}
+				
+				if (isInterrupted()) break;	
+			}
+			runCount.decrementAndGet();
+			showRun();
+			checkDone();
+		}
+		
+		public void showRun() {
+			SwingUtilities.invokeLater(new Runnable() {
+				public void run() {
+					running.setText("Running:" + runCount);
+				}
+			});
+		}
+
+	}
+	
+	public void loadFile() {
+		try {
+			BufferedReader in = new BufferedReader(new FileReader(FILENAME));
+
+			String line;
+			while ((line = in.readLine()) != null) {
+				model.addRow(new String[]{line, ""});
+				urls.add(line);
+				numWorkers++;
+			} 
+			in.close();
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public static void main(String[] args) {
--- hw4/src/webLoaderAssign/WebWorker.java
+++ hw4/src/webLoaderAssign/WebWorker.java
@@ -3,12 +3,33 @@ package webLoaderAssign;
 import java.io.*;
 import java.net.*;
 import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.*;
 import javax.swing.*;
 
 public class WebWorker extends Thread {
-/*
-  This is the core web/download i/o code...
+	private String urlString;
+	private int rowToUpdate;
+	private WebFrame frame;
+	private String status;
+	
+	private static final String ERROR = "err";
+	private static final String INT = "interrupted";
+	
+	public WebWorker(String u, int r, WebFrame f) {
+		urlString = u;
+		rowToUpdate = r;
+		frame = f;
+	}
+	
+	public void run() {
+		frame.runCount.incrementAndGet();
+		frame.launcher.showRun();
+		download();
+		frame.releaseWorker(status, rowToUpdate);
+	}
+	
+	public void download() {
+		//  This is the core web/download i/o code...
  		InputStream input = null;
 		StringBuilder contents = null;
 		try {
@@ -23,25 +44,36 @@ public class WebWorker extends Thread {
 			input = connection.getInputStream();
 
 			BufferedReader reader  = new BufferedReader(new InputStreamReader(input));
-		
+
+			long startTime = System.currentTimeMillis();
+			int size = 0;
 			char[] array = new char[1000];
 			int len;
-			contents = new StringBuilder(1000);
+			contents = new StringBuilder(1000);	
 			while ((len = reader.read(array, 0, array.length)) > 0) {
 				contents.append(array, 0, len);
-				Thread.sleep(100);
-			}
-			
+				if (isInterrupted()) {
+					status = INT;
+					break;
+				}
+				size += len;
+				Thread.sleep(100);	
+			}		
+			long finishTime = System.currentTimeMillis();
+			status = new SimpleDateFormat("HH:mm:ss").format(new Date(finishTime)) + "  " + (finishTime - startTime) + "ms  " + size + "bytes";
 			// Successful download if we get here
 			
 		}
 		// Otherwise control jumps to a catch...
-		catch(MalformedURLException ignored) {}
-		catch(InterruptedException exception) {
-			// YOUR CODE HERE
-			// deal with interruption
+		catch(MalformedURLException ignored) {
+			status = ERROR;
 		}
-		catch(IOException ignored) {}
+		catch(InterruptedException exception) {
+			status = INT;
+		}
+		catch(IOException ignored) {
+			status = ERROR;
+		}
 		// "finally" clause, to close the input stream
 		// in any case
 		finally {
@@ -50,7 +82,7 @@ public class WebWorker extends Thread {
 			}
 			catch(IOException ignored) {}
 		}
+	}
 
-*/
 	
 }
